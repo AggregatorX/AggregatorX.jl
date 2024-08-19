d5db7fc303d2e318c5082b056bbc932c1ceb64aa
Magnus Lilledahl <mlilledahl@gmail.com>
2024-08-19 13:48:58 +0200

    Added commands for creating variables for load and grid

--- OptimizeAggregator.jl
+++ OptimizeAggregator.jl
@@ -7,7 +7,10 @@ export optimizeaggregator
 function optimizeaggregator(aggregator, optimizer)
 
     model = Model(optimizer)
+    
     # Set up variables
+    @variable(model, p_load[1:length(aggregator["Load"]), 1:aggregator["TimeStruct"].periods])
+    @variable(model, p_grid[1:length(aggregator["Grid"]), 1:aggregator["TimeStruct"].periods])
 
     # Set up objective function
 
