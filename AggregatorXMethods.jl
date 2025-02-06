6559f1037ee35e6375edf508abf617e5a996af56
Magnus Lilledahl <mlilledahl@gmail.com>
2025-02-06 09:32:55 +0100

    Added energy reserve functionality to FCReGroup which should now propagate the constraint to all resources in the group

--- AggregatorXMethods.jl
+++ AggregatorXMethods.jl
@@ -648,17 +648,35 @@ function set_optimization_constraints(model::Model, group::FCReGroup, aggregator
     @constraint(model, total_down_capacity_reserved <= group.down_capacity, base_name = "down-capacity-limit-FCReGroup" * string(group.id))
 
      # Activation 
-    # FFR markets have no activation component so this just ensures that the resources
-    # does not contribute activation to this group. Without this the resources could
-    # freely set the value of their activation parameter, and e.g. down-activate and
-    # freely get energy from 'nowhere'.
+
+    # This FCR group assumes no activation. This constraint ensures that the resources
+    # does not contribute activation to this group. Without any constraint on this
+    # variable, the resources could freely set the value of their activation parameter,
+    # and e.g. down-activate and freely get energy from 'nowhere'.
     for id in group.resources
         r = get_component(id,aggregator)
-        @constraint(model, r.up_activation[group.id] == 0, base_name = "no-activation-FFRGroup" * string(group.id))
-        @constraint(model, r.down_activation[group.id] == 0, base_name = "no-activation-FFRGroup" * string(group.id))
+        @constraint(model, r.up_activation[group.id] == 0, base_name = "no-activation-FCReGroup" * string(group.id))
+        @constraint(model, r.down_activation[group.id] == 0, base_name = "no-activation-FCReGroup" * string(group.id))
     end
 
     # Energy reserves
+
+    # Set energy_endurance to maximum of connected markets
+    energy_endurance = 0
+    for id in group.markets
+        m = get_component(id, aggregator)
+        energy_endurance = max(energy_endurance, m.energy_endurance)
+    end
+
+    # Set energy reserve requirement individually for each connected resource in the group.
+    for id in group.resources
+        r = get_component(id,aggregator)
+        base_name = "up-energy-reserve-FCReGroup-" * string(group.id) * "r" * string(id)
+        @constraint(model, r.up_energy_reserve[group.id] >= energy_endurance * r.up_capacity[group.id], base_name = base_name)
+
+        base_name = "down-energy-reserve-FCReGroup-" * string(group.id) * "r" * string(id)
+        @constraint(model, r.down_energy_reserve[group.id] >= energy_endurance * r.down_capacity[group.id], base_name = base_name)
+    end
 
     # Total energy reserves available in the connected resources
     total_up_energy_reserve = init_expr_array(N)
